45bec19e53f83be3ed6d936bcfe07316716fbd9b
naga <naga@nagame>
2020-10-30 15:23:09 -0400

    Created a new project using Java 11 and Spring boot

---
+++ src/main/java/com/naga/medev/DockerGitSprinBootService/DockerGitSprinBootServiceApplication.java
@@ -0,0 +1,17 @@
+package com.naga.medev.DockerGitSprinBootService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class DockerGitSprinBootServiceApplication {
+
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to Springboot app");
+		SpringApplication.run(DockerGitSprinBootServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naga/medev/DockerGitSprinBootService/DockerGitSprinBootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naga.medev.DockerGitSprinBootService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGitSprinBootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
