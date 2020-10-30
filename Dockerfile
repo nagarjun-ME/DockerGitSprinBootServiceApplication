25103ab27dcf80cd28894fadeacd238e456e11da
naga <naga@nagame>
2020-10-30 19:15:53 -0400

    new end points added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM openjdk:11
-ADD target/DockerGitSprinBootService-0.0.1-SNAPSHOT.jar DockerGitSprinBootService-0.0.1-SNAPSHOT.jar
+FROM java:8
+
 EXPOSE 8091
+
+ADD /target/DockerGitSprinBootService-0.0.1-SNAPSHOT.jar DockerGitSprinBootService-0.0.1-SNAPSHOT.jar
+
 
 ENTRYPOINT ["java", "-jar", "DockerGitSprinBootService-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/naga/medev/DockerGitSprinBootService/RestAPI/DockerGitSprinBootServiceRestAPI.java
+++ src/main/java/com/naga/medev/DockerGitSprinBootService/RestAPI/DockerGitSprinBootServiceRestAPI.java
@@ -2,6 +2,8 @@ package com.naga.medev.DockerGitSprinBootService.RestAPI;
 
 
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +12,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/dgs")
 public class DockerGitSprinBootServiceRestAPI {
 	
 	private Logger log=LoggerFactory.getLogger(DockerGitSprinBootServiceRestAPI.class);
 	
-	@GetMapping
+	@GetMapping("/welcome")
 	public String sayHello() {
 		
 		log.info("Inside hello method");
 		return "Welcome to RespAPI";
+		}
+	
+	@GetMapping("/ddate")
+	public String getDate() {
+		
+		log.info("Inside Get date method");
+//		String dt;
+//			try {
+//				
+//				dt="Today date : "+ new Date().toString();
+//				log.info(dt);
+//			} catch (Exception ex){
+//				
+//				log.error("Date Error"+ ex.getMessage());
+//				dt="Something went wrong with date";
+//			}
+//		return dt;
+		return "this is a date method";
 		}
 
 }
